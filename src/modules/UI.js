6c0bf749c1943e166175c670bbc8acfecbce1cde
nekohamja <100026903+nekohamja@users.noreply.github.com>
2023-10-21 00:21:35 +0800

    add validations + indicators

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -5,17 +5,52 @@ export default class UI {
     const form = document.querySelector("#form");
     const phoneInput = document.querySelector("#phone");
 
-    // intl-tel-input API
+    UI.handleInput();
+
     window.intlTelInput(phoneInput, {
       utilsScript:
         "https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
       separateDialCode: true,
     });
-
     form.addEventListener("submit", (e) => {
-      UI.clear();
+      e.preventDefault(); // prevent actually submitting form
       Form.evaluateForm();
-      e.preventDefault(); // prevent submitting form
+    });
+  }
+
+  static handleInput() {
+    const formInputs = document.querySelectorAll("[form-field]");
+    const formName = document.querySelector("#name");
+    const formEmail = document.querySelector("#email");
+    const formPhone = document.querySelector("#phone");
+    const formPassword = document.querySelector("#password");
+    const formConfirmPassword = document.querySelector("#confirm-password");
+
+    formInputs.forEach((formInput) => {
+      formInput.addEventListener("focus", () => {
+        formInput.parentNode.children[2].textContent = "";
+        formInput.classList.remove("invalid");
+      });
+    });
+    formPhone.addEventListener("focus", () => {
+      formPhone.parentNode.parentNode.children[2].textContent = "";
+      formPhone.classList.remove("invalid");
+    });
+
+    formName.addEventListener("blur", () => {
+      Form.evaluateNameInput();
+    });
+    formEmail.addEventListener("blur", () => {
+      Form.evaluateEmailInput();
+    });
+    formPhone.addEventListener("blur", () => {
+      Form.evaluatePhoneInput();
+    });
+    formPassword.addEventListener("blur", () => {
+      Form.evaluatePasswordInput();
+    });
+    formConfirmPassword.addEventListener("blur", () => {
+      Form.evaluateConfirmPasswordInput();
     });
   }
 
@@ -24,10 +59,19 @@ export default class UI {
     errorIndicator[index].textContent = message;
   }
 
-  static clear() {
-    const errorIndicator = document.querySelectorAll("[error-handler]");
-    errorIndicator.forEach((message) => {
-      message.textContent = "";
+  static clearAll() {
+    const formInputs = document.querySelectorAll("[form-field]");
+    const formPhone = document.querySelector("#phone");
+    formInputs.forEach((input) => {
+      input.value = "";
     });
+    formPhone.value = "";
+  }
+
+  static formSubmitted() {
+    // todo
+    // create form submission indicator here
+    // implement SASS
+    // implement 3d video bg api
   }
 }
--- src/modules/form.js
+++ src/modules/form.js
@@ -1,44 +1,105 @@
 import UI from "./UI";
 
 export default class Form {
-  constructor(name, email, password) {
-    this.name = name;
-    this.email = email;
-    this.password = password;
+  static evaluateForm() {
+    const errorIndicator = document.querySelectorAll("[error-handler]");
+    let errorCount = 0;
+
+    Form.evaluateNameInput();
+    Form.evaluateEmailInput();
+    Form.evaluatePhoneInput();
+    Form.evaluatePasswordInput();
+
+    errorIndicator.forEach((message) => {
+      if (message.textContent !== "") {
+        errorCount++;
+      }
+    });
+
+    if (errorCount === 0) {
+      console.log("form submitted");
+      UI.clearAll();
+    }
   }
 
-  static evaluateForm() {
+  static evaluateNameInput() {
     const formName = document.querySelector("#name");
-    const formEmail = document.querySelector("#email");
-    const formPassword = document.querySelector("#password");
-    const formConfirmPassword = document.querySelector("#confirm-password");
-
     if (formName.value === "" || formName.value === null) {
       UI.errorMessage(0, "Name is required.");
-    }
+      formName.classList.add("invalid");
+    } else formName.classList.add("valid");
+  }
+
+  static evaluateEmailInput() {
+    const formEmail = document.querySelector("#email");
+    const validityState = formEmail.validity;
+
     if (formEmail.value === "" || formEmail.value === null) {
       UI.errorMessage(1, "Email is required.");
-    }
+      formEmail.classList.add("invalid");
+    } else formEmail.classList.add("valid");
+
+    if (validityState.typeMismatch) {
+      formEmail.classList.add("invalid");
+      UI.errorMessage(1, "Enter correct email address.");
+    } else formEmail.classList.add("valid");
+  }
+
+  static evaluatePhoneInput() {
+    const formPhone = document.querySelector("#phone");
+
+    if (formPhone.value === "" || formPhone.value === null) {
+      UI.errorMessage(2, "Phone is required.");
+      formPhone.classList.add("invalid");
+      return;
+    } else formPhone.classList.add("valid");
+
+    if (!/^\d+$/.test(formPhone.value)) {
+      UI.errorMessage(2, "Please input a Phone number.");
+      formPhone.classList.add("invalid");
+    } else formPhone.classList.add("valid");
+  }
+
+  static evaluatePasswordInput() {
+    const formPassword = document.querySelector("#password");
+
     if (formPassword.value === "" || formPassword.value === null) {
-      UI.errorMessage(2, "Please enter password.");
+      UI.errorMessage(3, "Please enter password.");
+      formPassword.classList.add("invalid");
       return;
-    }
+    } else formPassword.classList.add("valid");
+
     if (formPassword.value.length <= 8) {
-      UI.errorMessage(2, "Password must be longer than 8 characters.");
+      UI.errorMessage(3, "Password must be longer than 8 characters.");
+      formPassword.classList.add("invalid");
       return;
-    }
+    } else formPassword.classList.add("valid");
+
     if (formPassword.value.length >= 20) {
-      UI.errorMessage(2, "Password must be less than 20 characters.");
+      UI.errorMessage(3, "Password must be less than 20 characters.");
+      formPassword.classList.add("invalid");
       return;
-    }
+    } else formPassword.classList.add("valid");
+
+    Form.evaluateConfirmPasswordInput();
+  }
+
+  static evaluateConfirmPasswordInput() {
+    const formConfirmPassword = document.querySelector("#confirm-password");
+    const formPassword = document.querySelector("#password");
+
     if (
       formConfirmPassword.value === "" ||
       formConfirmPassword.value === null
     ) {
-      UI.errorMessage(3, "Please confirm password.");
-    }
+      UI.errorMessage(4, "Please confirm password.");
+      formConfirmPassword.classList.add("invalid");
+      return;
+    } else formConfirmPassword.classList.add("valid");
+
     if (formConfirmPassword.value !== formPassword.value) {
-      UI.errorMessage(3, "Passwords do not match.");
-    }
+      UI.errorMessage(4, "Passwords do not match.");
+      formConfirmPassword.classList.add("invalid");
+    } else formConfirmPassword.classList.add("valid");
   }
 }
